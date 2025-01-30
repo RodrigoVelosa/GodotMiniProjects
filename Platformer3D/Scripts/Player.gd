52fcb1526850a584b248cd0999cccce3b4420ec6
Rodrigo Velosa <rodrigofvelosa@gmail.com>
2025-01-30 22:52:16 +0000

    Created player on platform 3D game

---
+++ Platformer3D/Scripts/Player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody3D
+
+var move_speed: float = 4.0
+var jump_force: float = 8.0
+var gravity: float = 20.0
+
+var facing_angle: float
+
+@onready var model: MeshInstance3D = $Model
+
+func _physics_process(delta: float) -> void:
+	# apply gravity if we're in the air
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	
+	# get keyboard input
+	var input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	
+	# calculate move direction
+	var dir = Vector3(input.x, 0, input.y)
+	
+	# assign that direction to velocity
+	velocity.x = dir.x * move_speed
+	velocity.z = dir.z * move_speed
+	
+	# apply velocity
+	move_and_slide()
+	
+	# if we are moving, set facing direction
+	if input.length() > 0:
+		facing_angle = Vector2(input.y, input.x).angle()
+	
+	# rotate model to facing direction
+	model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.5)
+	
+	if global_position.y < -5:
+		game_over()
+	
+func game_over() -> void:
+	get_tree().reload_current_scene()
