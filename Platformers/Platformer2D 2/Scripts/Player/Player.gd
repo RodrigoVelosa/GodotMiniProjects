0f9f3fe79e0fd94a78ad2ca628ba45a267b16051
Rodrigo Velosa <rodrigofvelosa@gmail.com>
2025-02-07 22:46:31 +0000

    Start of machine state for platformer 2d 2

--- Platformers/Platformer2D 2/Scripts/Player/Player.gd
+++ Platformers/Platformer2D 2/Scripts/Player/Player.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var max_speed: int = 8000
+var acceleration: int = 1000
+var jump_height: int = 15000
+var direction: int = 0
 
+var friction: float = 0.22
+var weight: float = 0.35
+
+var current_state: String = ""
+var can_dash: bool = true
+var max_jumps: int = 3
+var jump_count: int = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		var target_vel: float = min(velocity.y + acceleration * delta, max_speed * delta)
+		velocity += lerp(velocity.y, target_vel, 0.6)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if direction < 0:
+		anim.flip_h = false
+	elif direction > 0:
+		anim.flip_h = true
 	move_and_slide()
