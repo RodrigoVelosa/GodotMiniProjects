0d71ceef7c0c13dbd5d6e6e3e175796e0fddcb3d
Rodrigo Velosa <rodrigofvelosa@gmail.com>
2025-04-28 23:06:36 +0100

    Abrir vs code

--- Tests/Main.gd
+++ Tests/Main.gd
@@ -4,3 +4,8 @@ func _on_button_pressed() -> void:
 	var output = []
 	var exit_code = OS.execute("powershell", ["-Command", "gc README.md", "-Tail 1"], output)
 	print(output)
+
+
+func _on_button_2_pressed() -> void:
+	var output = []
+	var exit_code = OS.execute("powershell", ["-Command", "code ."], output, false, true)
